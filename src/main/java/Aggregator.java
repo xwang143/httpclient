af2dfb6780e15d38c1c6e08fccdddb20beb7b5ec
xwang143 <xwang143@brandeis.edu>
2022-04-27 16:10:04 -0400

    client added

--- src/main/java/Aggregator.java
+++ src/main/java/Aggregator.java
@@ -1,3 +1,27 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Michael Pogrebinsky - Distributed Systems & Cloud Computing with Java
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 import networking.WebClient;
 
 import java.util.List;
@@ -11,19 +35,20 @@ public class Aggregator {
     public Aggregator() {
         this.webClient = new WebClient();
     }
-
+    // Takes a serie of http worker server addresses and a list of tasks， return aggregated responses
     public List<String> sendTasksToWorkers(List<String> workersAddresses, List<String> tasks) {
         CompletableFuture<String>[] futures = new CompletableFuture[workersAddresses.size()];
-        for (int i = 0; i < workersAddresses.size(); i++) {
-            String worker = workersAddresses.get(i);
-            String task = tasks.get(i);
-            byte[] payload = task.getBytes();
 
-            futures[i] = webClient.sendTask(worker, payload);
+        for (int i = 0; i < workersAddresses.size(); i++) {
+            String workerAddress = workersAddresses.get(i);
+            String task = tasks.get(i);
+
+            byte[] requestPayload = task.getBytes();
+            futures[i] = webClient.sendTask(workerAddress, requestPayload);
         }
 
-        List<String> results = Stream.of(futures).map(CompletableFuture::join)
-                .collect(Collectors.toList());
+        List<String> results = Stream.of(futures).map(CompletableFuture::join).collect(Collectors.toList());
+
         return results;
     }
 }
